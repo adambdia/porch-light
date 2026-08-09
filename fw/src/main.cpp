ec4d1594288b3db861b6ef3c07dfd2baa2d81a1b
Adam Dia <adambilaldia@protonmail.com>
2026-08-09 19:35:51 -0400

    secrets.h in gitignore

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 
 #define LEDPIN 8
+#define PERIOD 250
 
 void setup() { pinMode(LEDPIN, OUTPUT); }
 
 void loop() {
   digitalWrite(LEDPIN, HIGH);
-  delay(500);
+  delay(PERIOD);
   digitalWrite(LEDPIN, LOW);
   delay(500);
 }
