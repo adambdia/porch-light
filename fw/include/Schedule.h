cff903f742126adb592049c79d8914cae64c3a39
Adam Dia <adambilaldia@protonmail.com>
2026-08-15 01:04:18 -0400

    schedule not hardcoded to sunset/sunrise, no way of customizing yet

--- fw/include/Schedule.h
+++ fw/include/Schedule.h
@@ -1,7 +1,10 @@
+#include <time.h>
+
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
 
 void initSchedule();
+time_t getTomorrowAt(time_t base, int hour, int minute, int second);
 void loopSchedule();
 
 #endif
--- fw/src/Schedule.cpp
+++ fw/src/Schedule.cpp
@@ -1,33 +1,70 @@
 #include "Schedule.h"
 #include "APIClient.h"
 #include "Light.h"
-#include "esp32-hal.h"
 #include <Arduino.h>
 #include <cstdint>
 #include <ctime>
 #include <time.h>
 
 static const char *tz_info = "EST5EDT,M3.2.0,M11.1.0";
-static time_t sunset = 0;
-static time_t sunrise = 0;
+
+static time_t turnOffTime = 0;
+static time_t turnOnTime = 0;
+static bool useSunrise = true;
+static bool useSunset = true;
+
+static int tomorrowOffHour = 0;
+static int tomorrowOffMinute = 0;
+static int tomorrowOffSecond = 0;
+
+static int tomorrowOnHour = 0;
+static int tomorrowOnMinute = 0;
+static int tomorrowOnSecond = 0;
+
 static time_t syncTime = 0;
 static const uint32_t SCHEDULE_LOOP_INTERVAL = 1000;
 static uint32_t lastRunTime = 0;
 
-void updateSchedule() { fetchSunTimes(&sunrise, &sunset); }
+// might not set on/off time to follow sunset, this accounts for that
+void updateSchedule(time_t now) {
+  time_t sunrise;
+  time_t sunset;
 
-time_t getNextSyncTime(time_t now) {
+  if (useSunset || useSunset)
+    fetchSunTimes(&sunrise, &sunset);
+
+  if (useSunrise)
+    turnOffTime = sunrise;
+  else
+    turnOffTime = getTomorrowAt(now, tomorrowOffHour, tomorrowOffMinute,
+                                tomorrowOffSecond);
+
+  if (useSunset)
+    turnOnTime = sunset;
+  else
+    turnOnTime =
+        getTomorrowAt(now, tomorrowOnHour, tomorrowOnMinute, tomorrowOnSecond);
+}
+
+// helper function to get a timestamp the next day at a certain
+// hour:minute:second
+time_t getTomorrowAt(time_t base, int hour, int minute, int second) {
   // convert unix epoch to broken down time
-  struct tm *timeinfo = localtime(&now);
+  struct tm *timeinfo = localtime(&base);
 
-  // next day, 10 minutes past midnight
+  // next day plus offsets
   timeinfo->tm_mday += 1;
-  timeinfo->tm_hour = 0;
-  timeinfo->tm_min = 10;
-  timeinfo->tm_sec = 0;
+  timeinfo->tm_hour = hour;
+  timeinfo->tm_min = minute;
+  timeinfo->tm_sec = second;
 
   // converts the broken down time to unix time
   return mktime(timeinfo);
+}
+
+void getNextSyncTime(time_t now) {
+  // next day, 10 minutes past midnight
+  syncTime = getTomorrowAt(now, 0, 10, 0);
 }
 
 // config time and wait for first sync
@@ -39,26 +76,27 @@ void initSchedule() {
     now = time(nullptr);
   }
 
-  updateSchedule();
+  updateSchedule(now);
   getNextSyncTime(now);
 }
 
 void loopSchedule() {
+  // no reason to check faster than the clock updates
   if (millis() - lastRunTime < SCHEDULE_LOOP_INTERVAL)
     return;
 
   time_t now = time(nullptr); // current time in unix time
 
   if (now > syncTime) {
-    updateSchedule();
+    updateSchedule(now);
     getNextSyncTime(now);
   }
 
-  if (now < sunrise) {
+  if (now < turnOffTime) {
     scheduleLightOn();
-  } else if ((now > sunrise) && (now < sunset)) {
+  } else if ((now > turnOffTime) && (now < turnOnTime)) {
     scheduleLightOff();
-  } else if (now > sunset) {
+  } else if (now > turnOnTime) {
     scheduleLightOn();
   }
 
