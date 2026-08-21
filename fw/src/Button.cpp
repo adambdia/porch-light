52df173e09ed1ef6b458d94f7075b6df84cbf557
Adam Dia <adambilaldia@protonmail.com>
2026-08-21 19:15:32 -0400

    correctly debouncing button

--- fw/src/Button.cpp
+++ fw/src/Button.cpp
@@ -9,15 +9,22 @@ static const int BUTTON_ON = 0; // active low
 static const int BUTTON_OFF = 1;
 static const uint32_t BUTTON_DEBOUNCE_DURATION = 50; // 50 ms debounce
 static uint32_t lastButtonPress = 0;
+static int lastButtonState = BUTTON_OFF;
 
 void initButton() { pinMode(buttonPin, INPUT_PULLUP); }
 
 void loopButton() {
-  int buttonState = digitalRead(buttonPin);
-  if (buttonState == BUTTON_ON) {
-    if (millis() - lastButtonPress <= BUTTON_DEBOUNCE_DURATION)
-      return;
+  // debounce
+  if (millis() - lastButtonPress < BUTTON_DEBOUNCE_DURATION)
+    return;
 
-    overrideLight();
+  int currentButtonState = digitalRead(buttonPin);
+  // edge detector
+  if (currentButtonState != lastButtonState) {
+
+    lastButtonPress = millis();
+    lastButtonState = currentButtonState;
+    if (currentButtonState == BUTTON_ON)
+      overrideLight();
   }
 }
